f59a3c1d9e576226bef721f6d118e621510d440e
herobrine2231 <72050892+herobrine2231@users.noreply.github.com>
2021-01-16 12:23:06 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,37 +11,40 @@ var monkeyPlayer;
 var obstacleSprite;
 var obstacleGroup;
 var food, foodGroup;
-var score;
+var score=0;
 
 function preload()
 {
   jungleImage=loadImage("images/jungle.jpg");
   bananaImage=loadImage("images/banana.png");
-  /*moneky_running_image= loadAnimation("images/Monkey_01.png", 
+  moneky_running_image= loadAnimation("images/Monkey_01.png", 
   "images/Monkey_02.png", "images/Monkey_03.png", "images/Monkey_04.png", 
   "images/Monkey_05.png", "images/Monkey_06.png", "images/Monkey_07.png",
   "images/Monkey_08.png", "images/Monkey_09.png", "images/Monkey_10.png"
-  );*/
+  );
   monkeyImage=loadImage("images/Monkey_01.png");
 
   obstacleImage=loadImage("images/stone.png");
 }
 function setup()
 {
-    canvas= createCanvas(displayWidth-50, displayHeight-30);
-    jungleSprite=createSprite(displayWidth/2,400,0,0);
+    canvas= createCanvas(displayWidth, displayHeight);
+    jungleSprite=createSprite(displayWidth ,displayHeight,displayWidth,2);
     jungleSprite.addImage(jungleImage);
-   // jungleSprite.velocityX=-4;
+    jungleSprite.x=width/2;
+    jungleSprite.velocityX=-2;
 
-    floor1=createSprite(200,400,400,10);
-    floor1.visible=false;
+    floor1=createSprite(displayWidth/2,height,width,10);
+      floor1.visible=false;
 
-    monkeyPlayer=createSprite(210,600,10,10);
-    monkeyPlayer.addImage(monkeyImage);
+    monkeyPlayer=createSprite(100,height-70,20,50);
+    monkeyPlayer.addAnimation("monkey", moneky_running_image);
     monkeyPlayer.scale=0.1;
 
     foodGroup= new Group();
-    obstacleSprite= new Group();
+    obstacleGroup= new Group();
+
+    //score=0;
 
 
    
@@ -50,26 +53,29 @@ function setup()
 function draw()
 {
   //  background("red");
-  spawnFood();
-    if(keyDown("space"))
+
+  
+    if(keyDown("SPACE") || touches.length>0 )
     {
       monkeyPlayer.velocityY=-10;
+      touches= [];
     }
-    monkeyPlayer.velocityY=monkeyPlayer.velocityY+1;
+    monkeyPlayer.velocityY=monkeyPlayer.velocityY+2;
     monkeyPlayer.collide(floor1);
   spawnObstacles();
-  background(220);
-  if(jungleSprite.x<210)
+  spawnFood();
+ // background(jungleImage);
+  if(jungleSprite.x<0)
     {
-      jungleSprite.x=jungleSprite.displayWidth/2;
+      jungleSprite.x=jungleSprite.width/2;
     }
 
     if(foodGroup.isTouching(monkeyPlayer))
     {
-      score=score+2;
+      score= score+2
       food.remove();
     }
-
+      console.log(score);
     if(obstacleGroup.isTouching(monkeyPlayer))
     {
       monkeyPlayer.scale=0.1;
@@ -78,16 +84,16 @@ function draw()
     switch(score)
     {
       case 10:
-          monkeyPlayer.scale=0.12;
+          monkeyPlayer.scale=0.14;
           break;
         case 20:
-            monkeyPlayer.scale=0.14;
+            monkeyPlayer.scale=0.18;
             break;
         case 30:
-            monkeyPlayer.scale=0.16;
+            monkeyPlayer.scale=0.22;
             break;
         case 40:
-            monkeyPlayer.scale=0.18;
+            monkeyPlayer.scale=0.26;
             break;
         default:break;
     }
@@ -95,35 +101,35 @@ function draw()
     stroke("white");
     textSize(20);
     fill("white");
-    text("Score: " +score, 500,50);
+    text("Score: " +score, width-300,height-70);
 }
 
 function spawnObstacles()
 {
-  if(camera.position.x%80 ===0)
+  if(frameCount % 80 === 0)
   {
-    obstacleSprite= createSprite(310, 600, 40,10);
-    obstacleSprite.y= Math.round(random(250,370));
+    obstacleSprite= createSprite(600, height-95, 20,30);
+    obstacleSprite.y= Math.round(random(height,height-20));
     obstacleSprite.addImage(obstacleImage);
-    obstacleSprite.sc=0.05;
-  // obstacleSprite.velocityX=-3;
+    obstacleSprite.scale=0.05;
+   obstacleSprite.velocityX=-3;
     
-    obstacleSprite.lifetime=100;
+    obstacleSprite.lifetime=200;
     obstacleGroup.add(obstacleSprite);
   }
 }
 
 function spawnFood()
 {
-  if (camera.position.x%60===0)
+  if (frameCount % 60 === 0)
   {
-     food=createSprite(310,600,40,10);
-     food.y=Math.round(random(250,370));
+     food=createSprite(600,height-200,20,10);
+     food.y=Math.round(random(height-50,height-100));
     food.addImage(bananaImage);
     food.scale=0.05;
-   // food.velocityX=-3;
+    food.velocityX=-3;
 
-    food.lifetime=100;
+    food.lifetime=200;
     foodGroup.add(food);
   }
 }
